030743388031b49c821c171de6bb76b4c54ec211
beno santos <benosantos20008@gmail.com>
2024-01-15 10:53:16 -0300

    atualização

---
+++ funcoes/exercicio01.js
@@ -0,0 +1,23 @@
+function escrevaMeuNome(nome){
+    console.log('Meu nome é:' + nome);
+}
+
+function escrevaMeuSobrenome(sobrenome){
+    console.log('Meu sobrenome é:' + sobrenome);
+}
+
+function main(){
+    escrevaMeuNome(' Beno ');
+    escrevaMeuSobrenome( ' Santos ');
+}
+
+main();
+
+
+/*
+function escrevaMeuNome(nome){
+    console.log('Meu nome é:' + nome);
+}
+
+escrevaMeuNome(' Beno ')
+*/
---
+++ funcoes/exercicio02.js
@@ -0,0 +1,10 @@
+function verificarIdade(idade){
+    if(idade >= 18){
+        console.log('Sou maior de idade!');
+    }else{
+        console.log('Sou menor de idade!');
+    }
+}
+
+verificarIdade('22');
+
